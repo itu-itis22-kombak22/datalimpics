823a42465a570b68b76c53d34c0069714e783387
itu-itis21-bilginy21 <bilginy21@itu.edu.tr>
2024-11-18 11:49:31 +0300

    Brought back some deleted files

--- site-flask/create_database.py
+++ site-flask/create_database.py
@@ -4,8 +4,32 @@ def init_sqlite_db():
     conn = sqlite3.connect('database.db')
     print("Opened database successfully")
 
+    conn.execute('DROP TABLE IF EXISTS medals_total')
+    conn.execute('''
+        CREATE TABLE medals_total (
+            Rank INTEGER NOT NULL,
+            Country_Code TEXT NOT NULL,
+            Gold_Medal INTEGER DEFAULT NULL,
+            Silver_Medal INTEGER DEFAULT NULL,
+            Bronze_Medal INTEGER DEFAULT NULL,
+            Total INTEGER DEFAULT NULL,
+            Country TEXT,
+            PRIMARY KEY (Country_Code, Rank),
+            FOREIGN KEY (Country_Code) REFERENCES countries (country_code)
+        )
+    ''')
+    print("medals_total table created successfully")
+    conn.execute('DROP TABLE IF EXISTS countries')
+    conn.execute('''
+        CREATE TABLE countries (
+            country_code TEXT NOT NULL,
+            country_name TEXT DEFAULT NULL,
+            PRIMARY KEY (country_code)
+        )
+    ''')
+    print("countries table created successfully")
     # Only drop and create new tables
-    conn.execute('DROP TABLE IF EXISTS technical_officials')
+
     conn.execute('DROP TABLE IF EXISTS disciplines')
 
     # Create the disciplines table
@@ -16,7 +40,7 @@ def init_sqlite_db():
         )
     ''')
     print("disciplines table created successfully")
-
+    conn.execute('DROP TABLE IF EXISTS technical_officials')
     # Create the technical_officials table
     conn.execute('''
         CREATE TABLE technical_officials (
